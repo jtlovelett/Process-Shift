b28aea262904e87162ad2ee581e3f354b58736fd
jtlovelett <jtlovelett@gmail.com>
2018-07-03 11:27:53 -0700

    try to speedup delayed exp fitting
    - lower iter
    - lower max treedepth
    - lower adapt_delta

--- scripts/fitStanDE_ssdre.R
+++ scripts/fitStanDE_ssdre.R
@@ -62,11 +62,11 @@ fit.de <- stan(
   #model_code = mod,  # Stan program
   data = stan.data,    # named list of data
   chains = 4,             # number of Markov chains
-  warmup = 1000,          # number of warmup iterations per chain
-  iter = 3000,            # total number of iterations per chain
+  warmup = 500,          # number of warmup iterations per chain
+  iter = 2500,            # total number of iterations per chain
   cores = 4,              # number of cores
   refresh = 250,          # show progress every 'refresh' iterations
-  control = list(adapt_delta = 0.99,  max_treedepth = 15)
+  control = list(adapt_delta = 0.9,  max_treedepth = 12)
 
 )
 

1cab64126dc260cabf3fcf3e30779c74ea98583a
jtlovelett <jtlovelett@gmail.com>
2020-05-01 17:42:11 -0700

    more work on data gen; added stan intro file

--- scripts_tap-count/_generate_DE-data.R
+++ scripts_tap-count/_generate_DE-data.R
@@ -1,20 +1,34 @@
 library(dplyr)
 library(ggplot2)
+
+
 #### Parameters ####
+
+# data level
+nsubs <- 20
+nitems <- 10
+ntrials <- 100
+
+# model parameter means
+A <- 0
+B <- 1
+tau <- 0
+rate <- .5
+
+# model parameter sds 
+
+
 #  general
+subjects <- 1:20
+items <- 1:10
 trials <- 1:100
-# algorithm
-alg_mu <- 1000
-alg_sd <- 50
-# retrieval
-ret_mu <- function(trial, Alpha, Beta, rate){
-  Alpha + Beta*trial^(-rate)
-}
-#ret_sd <- alg_sd
-## p_retrieval
-logistic <- function(x, A, B){
-  1/(1 + exp(-A*(x-B)))
-}
+
+
+
+
+
+
+
 
 # generator
 gen_ProcShift_RTs <- function(trials,
@@ -25,14 +39,14 @@ gen_ProcShift_RTs <- function(trials,
   
   p_ret <- logistic(trials, p_ret_A, p_ret_B)
   PS_dat <- tibble(trial = trials,
-                       p_ret = p_ret,
-                       strategy = ifelse(rbinom(length(trials),1,p_ret),
-                                         'ret',
-                                         'alg')) %>%
+                   p_ret = p_ret,
+                   strategy = ifelse(rbinom(length(trials),1,p_ret),
+                                     'ret',
+                                     'alg')) %>%
     group_by(strategy) %>%
     mutate(trial_of_strategy = row_number()) %>%
     ungroup() %>%
-  ### IS THIS WHAT'S MISSING ELSEWHERE? (i.e. trial should be counted within each strategy not accross)
+    ### IS THIS WHAT'S MISSING ELSEWHERE? (i.e. trial should be counted within each strategy not accross)
     rowwise() %>%
     mutate(rt = case_when(strategy == 'alg' ~ rnorm(1,alg_mu, alg_sd),
                           strategy == 'ret' ~ rnorm(1,
@@ -52,5 +66,5 @@ gen_ProcShift_RTs <- function(trials,
 }
 
 
-#gen_ProcShift_RTs(trials, alg_mu, alg_sd, ret_rate=.2, p_ret_A =.1, ret_Alpha = 250, plot =T)
+gen_ProcShift_RTs(trials, alg_mu, alg_sd, ret_rate=.2, p_ret_A =.1, ret_Alpha = 250, plot =T)
 
